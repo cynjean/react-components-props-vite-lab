c88ecf4e7e36f08618ba267d41dc741abb474bf3
cynjean <cynthia.2056@yahoo.com>
2026-06-17 01:03:17 -0500

    Lab completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import blogData from "../data/blog";
 import Header from "./Header";
-import About from "./About";
 import ArticleList from "./ArticleList";
-
+import About from "./About";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Header name={blogData.name}/>
       <About image={blogData.image} about={blogData.about} />
       <ArticleList posts={blogData.posts} />
